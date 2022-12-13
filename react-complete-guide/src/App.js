16151d54d8c9c0164b1b0c09433795dec62f3743
Lucas Hideki Minamihara dos Santos <lucas@Jobcenter.net>
2022-12-13 11:44:45 -0300

    third commit

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,10 +2,34 @@ import ExpenseItem from "./components/Expenseltem";
 import "./App.css";
 
 function App() {
+  const values = [
+    { id: "1 ", title: "First Challenge", link: "youtube.com.br/lucckszim" },
+    { id: "2", title: "Second Challenge", link: "youtube.com.br/felpsz" },
+    {
+      id: "3",
+      title: "Third Challenge",
+      link: "Youtube.com.br/spectroPlayer",
+    },
+  ];
+
   return (
     <div>
       <h2>Let´s get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        id={values[0].id}
+        title={values[0].title}
+        link={values[0].link}
+      ></ExpenseItem>
+      <ExpenseItem
+        id={values[1].id}
+        title={values[1].title}
+        link={values[1].link}
+      ></ExpenseItem>
+      <ExpenseItem
+        id={values[2].id}
+        title={values[2].title}
+        link={values[2].link}
+      ></ExpenseItem>
     </div>
   );
 }
